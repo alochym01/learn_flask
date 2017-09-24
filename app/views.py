d08c2c9ac9b5e1a68cdf9f49d128f3cbdf51113b
alochym01 <donguyenha@gmail.com>
2017-09-24 21:06:40 +0700

    finsish jinja templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import render_template, request
 
 
 @app.route('/')
 def index():
     return 'Home Page'
+
+
+@app.route('/home')
+def homepage():
+    name = request.args.get('name')
+    if not name:
+        name = '<unknown>'
+    return render_template('homepage.html', name=name)
